af974157132be21fe53a5c3bb59bcd3873b7a23f
patrykwieteska <patrykwieteskapw@gmail.com>
2026-04-08 10:24:12 +0200

    changed environment directory  name

--- frontend/src/app/core/services/auth.service.ts
+++ frontend/src/app/core/services/auth.service.ts
@@ -2,7 +2,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
-import {environment} from '../../../environment/environment';
+import {environment} from '../../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
