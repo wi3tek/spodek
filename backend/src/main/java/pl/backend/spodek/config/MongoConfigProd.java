1c9f6c8d65d3ecf73ace171765fb51039586da22
patrykwieteska <patrykwieteskapw@gmail.com>
2026-04-08 09:13:33 +0200

    feat: force mongo config via code for prod

---
+++ backend/src/main/java/pl/backend/spodek/config/MongoConfigProd.java
@@ -0,0 +1,27 @@
+package pl.backend.spodek.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
+@Profile("prod")
+public class MongoConfigProd {
+
+    @Value("${MONGO_URI}")
+    private String mongoUri;
+
+    @Bean
+    public MongoClient mongoClientProd() {
+        return MongoClients.create(mongoUri);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplateProd() {
+        return new MongoTemplate(mongoClientProd(), "spodek_prod");
+    }
+}
--- backend/src/main/java/pl/backend/spodek/config/SecurityConfig.java
+++ backend/src/main/java/pl/backend/spodek/config/SecurityConfig.java
@@ -5,12 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -24,7 +18,6 @@ public class SecurityConfig {
 
     private final UserRepository userRepository;
     private final JwtAuthenticationFilter jwtAuthFilter; // Wstrzykujemy nasz nowy filtr
-
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
