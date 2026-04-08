a66c65a766d16a8dfa692796b625222e87780c53
patrykwieteska <patrykwieteskapw@gmail.com>
2026-04-08 11:06:04 +0200

    fixed routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,7 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './features/auth/login/login'; // dopasuj ścieżkę do swojego pliku login.ts
+import { LoginComponent } from './features/auth/login/login';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }, // przekieruj pusty adres na logowanie
-  { path: '**', redirectTo: '/login' } // każdą nieznaną stronę przekieruj na logowanie
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
