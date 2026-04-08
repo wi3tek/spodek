6fa46bca4f670d34fed03d2b1228def68e15f77c
patrykwieteska <patrykwieteskapw@gmail.com>
2026-04-08 10:46:14 +0200

    downgrade to angular 19

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login'; // dopasuj ścieżkę do swojego pliku login.ts
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // przekieruj pusty adres na logowanie
+  { path: '**', redirectTo: '/login' } // każdą nieznaną stronę przekieruj na logowanie
+];
