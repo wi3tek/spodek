d28e863b991e6b209ec137a6febd5d12dc0ff4c6
patrykwieteska <patrykwieteskapw@gmail.com>
2026-04-10 00:09:49 +0200

    fixed loader

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LoadingSpinnerComponent} from './features/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LoadingSpinnerComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
