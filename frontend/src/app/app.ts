cf189628de63f5639d1cae23a5a447f5d9dc833f
patrykwieteska <patrykwieteskapw@gmail.com>
2026-07-20 00:40:51 +0200

    automatyczne aktualizacje

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,7 +1,9 @@
-import { Component, signal, effect } from '@angular/core';
+import {Component, signal, effect, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoadingSpinnerComponent } from './features/loading-spinner/loading-spinner.component';
 import { CommonModule } from '@angular/common';
+import {SwUpdate, VersionReadyEvent} from '@angular/service-worker';
+import {filter} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +14,7 @@ import { CommonModule } from '@angular/common';
 })
 export class App {
   protected readonly title = signal('spodek-ui');
-
+  private swUpdate = inject(SwUpdate);
   // ODCZYT: Sprawdzamy czy w localStorage zapisano włączony motyw
   isWorldCupTheme = signal<boolean>(localStorage.getItem('theme_worldcup') === 'true');
 
@@ -35,5 +37,25 @@ export class App {
   // Funkcja wywoływana kliknięciem przycisku (zmienia stan na odwrotny)
   toggleTheme() {
     this.isWorldCupTheme.update((active) => !active);
+  }
+
+  ngOnInit() {
+    // Upewniamy się, że Service Worker jest włączony (np. działa tylko na produkcji)
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates
+        .pipe(
+          // Filtrujemy zdarzenia, interesuje nas tylko moment, gdy nowa wersja jest gotowa
+          filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY')
+        )
+        .subscribe(() => {
+          // Opcja 1: Automatyczne (agresywne) odświeżenie bez pytania
+          // window.location.reload();
+
+          // Opcja 2: (Zalecana) Pytamy użytkownika, czy chce zaktualizować
+          if (confirm('Dostępna jest nowa wersja aplikacji. Czy chcesz zaktualizować teraz?')) {
+            window.location.reload();
+          }
+        });
+    }
   }
 }
